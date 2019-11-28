93c371afe7777ae7f798f236e317cc8a67b1c3ad
AndryKot <andry.kot.9@gmail.com>
2019-11-28 21:03:50 +0200

    add new PingDashboardActivity

---
+++ app/src/main/java/com/itea/practice/fundamentals/tasks/components/PingDashboardActivity.java
@@ -0,0 +1,17 @@
+
+package com.itea.practice.fundamentals.tasks.components;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.itea.practice.fundamentals.R;
+
+public class PingDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ping_dashboard);
+    }
+}
